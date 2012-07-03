129cfde1fe5b59b80cc565bf7cca9fadf7e3ccad
Joao Barreiro <jbarlx@gmail.com>
2012-07-03 18:28:41 +0200

    adding feature

--- features/step_definitions/calculator_steps.rb
+++ features/step_definitions/calculator_steps.rb
@@ -1,9 +1,10 @@
 Given /^the input "(.*?)"$/ do |arg1|
-  pending # express the regexp above with the code you wish you had
+  @input = arg1
 end
 
 When /^the calculator is run$/ do
-  pending # express the regexp above with the code you wish you had
+  @output = `ruby calc.rb #{@input}`
+  raise('Command failed') unless $?.success?
 end
 
 Then /^the output should be "(.*?)"$/ do |arg1|
